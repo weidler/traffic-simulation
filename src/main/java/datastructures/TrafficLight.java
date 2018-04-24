3ae7bb4e9c197f883ce6b6202acc12a2f05f130b
vincevandriel <vincevandriel@gmail.com>
2018-04-24 18:59:49 +0200

    Traffic Light List (working!)

--- src/main/java/datastructures/TrafficLight.java
+++ src/main/java/datastructures/TrafficLight.java
@@ -7,7 +7,8 @@ public class TrafficLight {
 	private Road road;
 	private int lanes;
 	private Intersection intersection;
-	private ArrayList<TrafficLight> TrafficLightList;
+	private ArrayList<TrafficLight> TrafficLightList = new ArrayList<TrafficLight>();
+	private boolean inProgress = false;
 
 	public TrafficLight(Road road, Intersection intersection, int lanes) {
 		this.road = road;
@@ -15,7 +16,7 @@ public class TrafficLight {
 		this.lanes = lanes;
 		this.status = "R";
 		if(lanes > 1) {
-//			addTrafficLightToLanes(road,intersection,lanes);
+			addTrafficLightToLanes(road,intersection,lanes);
 		}
 	}
 
@@ -37,18 +38,19 @@ public class TrafficLight {
 	}
 	
 	//If multiple lanes, create an arrayList of traffic lights for the road
-//	public ArrayList<TrafficLight> addTrafficLightToLanes(Road road, Intersection intersection, int lanes){
-//		TrafficLightList = new ArrayList<TrafficLight>();
-//		for(int i = 0; i < lanes; i++) {
-//			TrafficLightList.add(new TrafficLight(road,intersection,lanes));
-//		}
-//		return TrafficLightList;
-//	}
-//	
-//	public ArrayList<TrafficLight> getTrafficLightList(){
-//		return TrafficLightList;
-//	}
-//	
+	public ArrayList<TrafficLight> addTrafficLightToLanes(Road road, Intersection intersection, int lanes){
+		for(int i = 0; i < lanes; i++) {
+			lanes = lanes - 1;
+			TrafficLightList.add(new TrafficLight(road,intersection,lanes));
+		}
+		System.out.println(TrafficLightList);
+		return TrafficLightList;
+	}
+	
+	public ArrayList<TrafficLight> getTrafficLightList(){
+		return TrafficLightList;
+	}
+	
 	public Intersection getIntersection()
 	{
 		return intersection;
