ab393c3b75b13e28780defb74d61da5e7bcd948d
vincevandriel <vincevandriel@gmail.com>
2018-04-24 17:55:29 +0200

    Traffic Light List

--- src/main/java/datastructures/TrafficLight.java
+++ src/main/java/datastructures/TrafficLight.java
@@ -1,17 +1,19 @@
 package datastructures;
 
+import java.util.ArrayList;
+
 public class TrafficLight {
 	private String status;
 	private Road road;
 	private int lanes;
 	private Intersection intersection;
+	private ArrayList<TrafficLight> TrafficLightList;
 
 	public TrafficLight(Road road, Intersection intersection, int lanes) {
 		this.road = road;
 		this.intersection = intersection;
 		this.lanes = lanes;
 		this.status = "R";
-		
 		if(lanes > 1) {
 			for (int i = 0; i < lanes; i++) {
 				
@@ -35,6 +37,15 @@ public class TrafficLight {
 		}
 		this.status = status;
 	}
+	
+	public ArrayList addTrafficLightToLanes(Road road, Intersection intersection, int lanes){
+		TrafficLightList = new ArrayList<TrafficLight>();
+		for(int i = 0; i < lanes; i++) {
+			TrafficLightList.add(new TrafficLight(road,intersection,lanes));
+		}
+		return TrafficLightList;
+	}
+	
 	public Intersection getIntersection()
 	{
 		return intersection;
